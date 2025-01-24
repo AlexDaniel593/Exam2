29358fd774ca897d797fded6e7798a93265a0f91
kfamaguana <kfamaguana@espe.edu.ec>
2025-01-24 12:14:16 -0500

    Funcion Mostrar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-// script
+//Creacion libros
+const books = [
+    { id: 1, title: "JavaScript: The Good Parts", author: "Douglas Crockford", genre: "Programming", available: true },
+    { id: 2, title: "Eloquent JavaScript", author: "Marijn Haverbeke", genre: "Programming", available: true },
+    { id: 3, title: "Clean Code", author: "Robert C. Martin", genre: "Programming", available: true },
+];
+
+const borrowedBooks = [];
+const notifications = [];
+
+// Mostrar lista de libros
+function mostrarLibros() {
+    let text = '';
+    for (const book of books) {
+        text += `ID: ${book.id} <br> Title: ${book.title} <br> Author: ${book.author} <br> Genre: ${book.genre} <br> Available: ${book.available} <br><br>`;
+    }
+    document.getElementById('book-list').innerHTML = text;
+}
+
